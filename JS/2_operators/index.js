00a0cec9cc7646b73595adda3506de760310f5b6
zstech103-droid <zs00483@gmail.com>
2026-06-26 05:14:22 +0530

    day5

---
+++ JS/2_operators/index.js
@@ -0,0 +1,28 @@
+
+// arithmetic operators = operands (values, variables, etc.)
+//                        operators (+ - * /)
+//                        ex. 11 = x + 5;
+
+let students = 30;
+
+//students = students + 1;
+//students = students - 1;
+//students = students * 2;
+//students = students / 2;
+//students = students ** 2;
+//let extraStudents = students % 3;
+
+console.log(students);
+//augmented assigning operators
+//students += 1;
+//students -= 1;
+//students *= 2;
+//students /= 2;
+//students **= 2;
+//students %= 2;
+
+//increment/decrement operator
+students++;
+students--;
+
+//operator precedence
